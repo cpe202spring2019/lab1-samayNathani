53bbca6af14dd53f4c47775fc4f14346c16cce45
Samay Nathani <46657835+samayNathani@users.noreply.github.com>
2019-04-09 20:01:02 -0700

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -18,21 +18,18 @@ def reverse_rec(int_list):
       r = [int_list.pop(-1)]
       return r + (reverse_rec(int_list))
 
-def bin_search(target, low, high, int_list):  # must use recursion
-   """searches for target in int_list[low..high] and returns index if found
-   If target is not found returns None. If list is None, raises ValueError """
+def bin_search(target, low, high, int_list):
    if int_list is None:
       raise ValueError
-   idx = None
-   while low <= high:
-      mid = (high + low)//2
-      if target == int_list[mid]:
-         return mid
+   if (high < low):
+      return None
+   mid = (high + low)//2
+   if target == int_list[mid]:
+      return mid
+   else:
+      if target < int_list[mid]:
+         high = mid - 1
+         return bin_search(target, low, high, int_list)
       else:
-         if target < int_list[mid]:
-            high = mid - 1
-            bin_search(target, low, high, int_list)
-         else:
-            low = mid + 1
-            bin_search(target, low, high, int_list)
-   return idx
+         low = mid + 1
+         return bin_search(target, low, high, int_list)
